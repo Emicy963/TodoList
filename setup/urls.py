ffa477f03503a4c10d69b41c4a134448e7337388
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-03-12 14:51:36 +0100

    Config API endpoints

--- setup/urls.py
+++ setup/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from todos.views import TodoListViews, TodoCreateView, TodoUpdateView, TodoDeleteView, TodoCompleteView
+from todos.api import TodoViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('todos', TodoViewSet, basename='todo')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,4 +14,6 @@ urlpatterns = [
     path("update/<int:pk>", TodoUpdateView.as_view(), name="todo_update"),
     path("delete/<int:pk>", TodoDeleteView.as_view(), name="todo_delete"),
     path("complete/<int:pk>", TodoCompleteView.as_view(), name="todo_complete"),
+    # API urls
+    path('api', include(router.urls)),
 ]
