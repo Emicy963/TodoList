4c937d9ffd924ac2e72bd0d6dad266a0d8d6dbda
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-03-22 22:42:14 +0100

    Update todos views for config user

--- todos/views.py
+++ todos/views.py
@@ -1,32 +1,39 @@
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, View
 from django.urls import reverse_lazy
 from django.shortcuts import get_object_or_404, redirect
-
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Todo
 
 
-class TodoListViews(ListView):
+class TodoListViews(LoginRequiredMixin, ListView):
     model = Todo
 
-class TodoCreateView(CreateView):
-    model = Todo
-    fields = ["title", "deadline"]
-    success_url = reverse_lazy('todo_list')
-
-class  TodoUpdateView(UpdateView):
+    def get_queryset(self):
+        return Todo.objects.filter(user=self.request.user)
+    
+class TodoCreateView(LoginRequiredMixin, CreateView):
     model = Todo
     fields = ["title", "deadline"]
     success_url = reverse_lazy('todo_list')
 
-class TodoDeleteView(DeleteView):
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+class  TodoUpdateView(LoginRequiredMixin, UpdateView):
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url = reverse_lazy('todo_list')
+
+class TodoDeleteView(LoginRequiredMixin, DeleteView):
     model = Todo
     success_url = reverse_lazy('todo_list')
     template_name = 'todos/todo_confirm_delete.html'
 
 
-class TodoCompleteView(View):
+class TodoCompleteView(LoginRequiredMixin, View):
     def get(self, request, pk):
-        todo = get_object_or_404(Todo, pk=pk)
+        todo = get_object_or_404(Todo, pk=pk, user=request.user)
         todo.mark_has_complete()
         return redirect("todo_list")
 
