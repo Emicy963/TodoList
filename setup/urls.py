0201d4dac26d9f6e24a2dd6449f05abfc20138d2
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-03-22 22:31:10 +0100

    Create a users apps, config and create a register views, urls for register, login and logout

--- setup/urls.py
+++ setup/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from todos.views import TodoListViews, TodoCreateView, TodoUpdateView, TodoDeleteView, TodoCompleteView
 from todos.api import TodoView, TodoDetailView, TodoCompleteAPI
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('users.urls')),
     path("", TodoListViews.as_view(), name="todo_list"),
     path("create", TodoCreateView.as_view(), name="todo_create"),
     path("update/<int:pk>", TodoUpdateView.as_view(), name="todo_update"),
